0a57819a5ae185ae9cfd89a7b731464c13b37719
isel tarek <iseltarek15@gmail.com>
2025-04-17 12:18:32 +0200

    create database entities.

---
+++ src/database/entities/users.entity.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+import { Role } from 'src/utlis/enum/user-role.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Result } from './results.entity';
+import { Quiz } from './quiz.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role })
+  role: Role;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Result, (result) => result.student)
+  results: Result[];
+
+  @OneToMany(() => Quiz, (quiz) => quiz.createdBy)
+  createdQuizzes: Quiz[];
+}
---
+++ src/utlis/enum/question-type.enum.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export enum QuestionType {
+  MULTIPLE_CHOICE = 'multiple_choice',
+  CHECKBOX = 'checkbox',
+  TEXT = 'text',
+}
